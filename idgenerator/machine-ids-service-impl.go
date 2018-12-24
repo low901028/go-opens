68e95f821fe56639d82cd799740eb8fa801efb6e
dalan <dalan_123@126.com>
2018-12-24 10:26:06 +0800

    更新MachineIdsServiceImpl部分方法

--- idgenerator/machine-ids-service-impl.go
+++ idgenerator/machine-ids-service-impl.go
@@ -21,6 +21,7 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -30,36 +31,31 @@ const STORE_FILE_NAME = "machineIdInfo.store"
 type MachineIdsIdServiceImpl struct {
 	IdServiceImpl
 	lastTimestamp int64
-	machineIdMap map[int64]int64
+	machineIdMap  map[int64]int64
 	storeFilePath string
-	storeFile os.File
+	storeFile     os.File
 	sync.Mutex
 }
 
-func (mid MachineIdsIdServiceImpl) init(){
-	if(!(reflect.TypeOf(mid.machineIdProvider).Name() == "MachineIdsProvider")){
+func (mid MachineIdsIdServiceImpl) init() {
+	if !(reflect.TypeOf(mid.machineIdProvider).Name() == "MachineIdsProvider") {
 		log.Fatal("The machineIdProvider is not a MachineIdsProvider instance so that Vesta Service refuses to start.")
 	}
-	initStoreFile()
-	initMachineId()
+	mid.initStoreFile()
+	mid.initMachineId()
 }
 
 func (mid MachineIdsIdServiceImpl) populateId(id Id) {
 	mid.supportChangeMachineId(id)
 }
 
-func supportChangeMachine(mid MachineIdsIdServiceImpl) error{
-
-	return nil
-}
-
 func (mid MachineIdsIdServiceImpl) supportChangeMachineId(id Id) {
-	if (id.getMachine() == mid.machineId){
+	if id.getMachine() == mid.machineId {
 		mid.Lock()
-		changeMachineId()
-		resetIdPopulator()
+		mid.changeMachineId()
+		mid.resetIdPopulator()
 		mid.Unlock()
-		supportChangeMachineId(id)
+		mid.supportChangeMachineId(id)
 	} else {
 		id.setMachine(mid.machineId)
 		mid.idPopulator.populateId(mid.timer, id, mid.idMeta)
@@ -69,40 +65,42 @@ func (mid MachineIdsIdServiceImpl) supportChangeMachineId(id Id) {
 
 func (mid MachineIdsIdServiceImpl) changeMachineId() {
 	mid.machineIdMap[mid.machineId] = mid.lastTimestamp
-	storeInFile()
-	initMachineId()
+	mid.storeInFile()
+	mid.initMachineId()
 }
 
 func (mid MachineIdsIdServiceImpl) resetIdPopulator() {
-	if (reflect.TypeOf(mid.idPopulator).Name() == "ResetPopulator") {
+	if reflect.TypeOf(mid.idPopulator).Name() == "ResetPopulator" {
 		(RestPopulator(mid.idPopulator.(RestPopulator))).reset()
 	} else {
-		var newIdPopulator= reflect.New(reflect.TypeOf(mid.idPopulator))
+		var newIdPopulator = reflect.New(reflect.TypeOf(mid.idPopulator))
 		mid.idPopulator = new(newIdPopulator.Type())
 	}
 }
 
 func (mid MachineIdsIdServiceImpl) initStoreFile() {
-	if (len(mid.storeFilePath) == 0) {
+	if len(mid.storeFilePath) == 0 {
 		mid.storeFilePath = os.Getenv("user.dir") + "/" + STORE_FILE_NAME
 	}
 	log.Printf("machineId info store in <[%s]>", mid.storeFilePath)
-	if ok , _ := PathExists(mid.storeFilePath); ok {
+	if ok, _ := PathExists(mid.storeFilePath); ok {
 		bytes, err := ioutil.ReadFile(mid.storeFilePath)
-		if err != io.EOF{
+		if err != io.EOF {
 			log.Fatal("read machineId info store file occur exception, ", err)
 		}
-		lines := strings.Split(string(bytes),"/n/r")
-		if len(lines) > 0{
-			for _, line := range lines{
+		lines := strings.Split(string(bytes), "/n/r")
+		if len(lines) > 0 {
+			for _, line := range lines {
 				kvs := strings.Split(line, ":")
-				if len(kvs) == 2{
-					if mid.machineIdMap == nil{
+				if len(kvs) == 2 {
+					if mid.machineIdMap == nil {
 						mid.machineIdMap = make(map[int64]int64)
 					}
-					mid.machineIdMap[int64(kvs[0])] = int64(kvs[1])
+					key, _ := strconv.ParseInt(kvs[0], 10, 64)
+					value, _ := strconv.ParseInt(kvs[1], 10, 64)
+					mid.machineIdMap[key] = int64(value)
 				} else {
-					log.Fatal(filepath.Abs(mid.storeFilePath) , " has illegal value <[" + line + "]>")
+					log.Fatal(filepath.Abs(mid.storeFilePath), " has illegal value <["+line+"]>")
 				}
 			}
 		}
@@ -113,16 +111,16 @@ func (mid MachineIdsIdServiceImpl) initMachineId() {
 	var startId int64 = mid.machineId
 	var newMachineId int64 = mid.machineId
 	for {
-		if (mid.machineIdMap[(newMachineId)] != 0 ) {
+		if mid.machineIdMap[(newMachineId)] != 0 {
 			timestamp := mid.timer.genTime()
-			if (mid.machineIdMap[newMachineId] < timestamp) {
+			if mid.machineIdMap[newMachineId] < timestamp {
 				mid.machineId = newMachineId
 				break
 			} else {
-				mip := MachineIdsProvider(mid.machineIdProvider)
-				newMachineId = (.getNextMachineId()
+				mip := mid.machineIdProvider.(MachineIdsProvider)
+				newMachineId = mip.getNextMachineId()
 			}
-			if (newMachineId == startId) {
+			if newMachineId == startId {
 				log.Fatal("No machineId is available")
 			}
 			mid.validateMachineId(newMachineId)
@@ -134,15 +132,15 @@ func (mid MachineIdsIdServiceImpl) initMachineId() {
 	log.Printf("MachineId: %d\n", mid.machineId)
 }
 
-func  (mid MachineIdsIdServiceImpl) storeInFile() {
+func (mid MachineIdsIdServiceImpl) storeInFile() {
 	var datas []string
-	for key, value := range mid.machineIdMap{
-		datas = append(datas, fmt.Sprintf("%d:%d",key, value))
+	for key, value := range mid.machineIdMap {
+		datas = append(datas, fmt.Sprintf("%d:%d", key, value))
 	}
-	data := strings.Join(datas,"\n")
-	ioutil.WriteFile(mid.storeFilePath, []byte(data),0644)
+	data := strings.Join(datas, "\n")
+	ioutil.WriteFile(mid.storeFilePath, []byte(data), 0644)
 }
 
-func  (mid MachineIdsIdServiceImpl) setStoreFilePath(storeFilePath string) {
+func (mid MachineIdsIdServiceImpl) setStoreFilePath(storeFilePath string) {
 	mid.storeFilePath = storeFilePath
 }
